e4d88049b4a6a924ddeb60321117e3db595b5020
BandaySajid <ibandaysajid@gmail.com>
2025-08-28 13:12:31 +0530

    working, removed length stuff

--- main.js
+++ main.js
@@ -30,11 +30,14 @@ async function takeScreenshotAndProcess() {
       model: "gemini-2.5-flash",
       contents: [
         createUserContent([
-          "Answer question shown in the image. Give a direct, response without extra details. For multiple-choice questions, respond only with the letter of the correct option and its answer. Do not add any unrelated information. If the question is a subjective question, elaborate it and answer everything that question asks, in long format or according to the question.",
+          "Answer question shown in the image. Give a direct, response without extra details. For multiple-choice questions, respond only with the letter of the correct option and its answer. Do not add any unrelated information. If the question is a subjective question, with no options answer everything that question asks, according to the question, not too long, not two short, make sure that it at least can be written on 1 A4 page, make sure its in the format of college exam answer. Make sure the answer is not too big, I have only 15 minutes to write the answer.",
+          // "Given the image and the caption text shown on screen, first extract the main question from the caption. Then, answer that question concisely as you would in a viva exam: keep the answer short, direct, and focused only on the essential points needed for a spoken response. Do not include extra details or unrelated information.",
           createPartFromUri(image.uri, image.mimeType),
         ]),
       ],
     });
+
+    console.log('response', response.text);
 
     if (win) {
       win.webContents.send("update-text", response.text);
@@ -94,4 +97,5 @@ app.whenReady().then(() => {
 
   app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
 });
+
 app.on('will-quit', () => { globalShortcut.unregisterAll(); });
